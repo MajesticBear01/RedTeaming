3754431aef50723cab9a2d933dcbb801411fe4a0
MajesticBear01 <rubyroguerecon@proton.me>
2024-09-24 14:47:26 -0600

    speaker.ps1

--- speaker.ps1
+++ speaker.ps1
@@ -1,3 +1,5 @@
 Function Speak-Text($Text) { Add-Type -AssemblyName System.speech; $TTS = New-Object System.Speech.Synthesis.SpeechSynthesizer; $TTS.Speak($Text) }
-Speak-Text "Please contact the IT you are hacked"
+Speak-Text "WARNING: Your files have been encrypted!
+All your important documents, photos, and data are now inaccessible. A ransomware attack has taken place, and your files are locked with strong encryption. To regain access, you must pay a ransom of $500 in cryptocurrency within the next 72 hours. Failure to comply will result in permanent deletion of your files.
+Do NOT attempt to recover your files without our assistance. Any attempts to restore or alter the system may result in irreversible loss."
 Exit
